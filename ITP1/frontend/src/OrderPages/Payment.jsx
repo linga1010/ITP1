c5e96b15255d8303467106bd8518780eaeed719c
Kaja-Code <skajanan2022@gmail.com>
2025-03-16 03:06:02 +0530

    order pages added

--- ITP1/frontend/src/OrderPages/Payment.jsx
+++ ITP1/frontend/src/OrderPages/Payment.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 
 const PaymentDetails = () => {
+     const navigate = useNavigate();
   return (
     <div>
       <h1>Payment Details </h1>
+
       {/* Display Payment Details  here */}
+
+<button Click={() => navigate("/order")} >Back </button>
     </div>
   );
+  
 };
 
 export default PaymentDetails;
